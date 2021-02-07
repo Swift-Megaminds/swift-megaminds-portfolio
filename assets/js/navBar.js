fffda0e340edbcde0726b844e6e713e78ff78943
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2021-02-07 14:13:49 +0300

    feat: hide nav bar on scroll

--- assets/js/navBar.js
+++ assets/js/navBar.js
@@ -1,7 +1,30 @@
 const toggleSwitch = document.getElementById("toggle-checkbox");
 const nav = document.querySelector(".nav");
 const toggleIcon = document.getElementById("toggle-icon");
-console.log(toggleIcon);
+
+console.log(nav);
+var new_scroll_position = 0;
+var last_scroll_position;
+
+window.addEventListener("scroll", function (e) {
+  console.log(e);
+  last_scroll_position = window.scrollY;
+
+  // Scrolling down
+  if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+    // nav.removeClass('slideDown').addClass('slideUp');
+    nav.classList.remove("slideDown");
+    nav.classList.add("slideUp");
+
+    // Scrolling up
+  } else if (new_scroll_position > last_scroll_position) {
+    // nav.removeClass('slideUp').addClass('slideDown');
+    nav.classList.remove("slideUp");
+    nav.classList.add("slideDown");
+  }
+
+  new_scroll_position = last_scroll_position;
+});
 
 const switchTheme = (event) => {
   const { checked } = event.target;
